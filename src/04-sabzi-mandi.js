cb74726a3cd85a39a677e0be8b5a2f2d869a1025
surya <varma.surya@protonmail.com>
2026-02-13 18:56:08 -0500

    add sabzi mandi source code

--- src/04-sabzi-mandi.js
+++ src/04-sabzi-mandi.js
@@ -49,20 +49,49 @@
  */
 export function addToCart(cart, item) {
   // Your code here
+  if (!Array.isArray(cart)) {
+    return -1;
+  }
+  if (typeof item != "string" || item.length == "") {
+    return cart.length;
+  }
+
+  const cartLength = cart.push(item);
+  return cartLength;
 }
 
 export function addUrgentItem(cart, item) {
   // Your code here
+  if (!Array.isArray(cart)) {
+    return [];
+  }
+  if (typeof item != "string" || item.trim() === "") {
+    return cart;
+  }
+  cart.unshift(item);
+  return cart;
 }
 
 export function removeLastItem(cart) {
   // Your code here
+  if (!Array.isArray(cart) || cart == []) {
+    return undefined;
+  }
+  return cart.pop();
 }
 
 export function isInCart(cart, item) {
   // Your code here
+  if (!Array.isArray(cart)) {
+    return false;
+  }
+  return cart.includes(item);
 }
 
 export function mergeCarts(cart1, cart2) {
   // Your code here
+  const firstCart = Array.isArray(cart1) ? cart1 : [];
+  const secondCart = Array.isArray(cart2) ? cart2 : [];
+
+  return firstCart.concat(secondCart);
 }
